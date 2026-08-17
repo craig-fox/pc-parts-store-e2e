01b187c915878e9a5dcb8e644771e691ffedc69a
Craig Fox <craig.fox@icloud.com>
2026-08-17 18:36:43 +1200

    Initial working setup

---
+++ src/test/java/nz/fox/craig/e2e/RunCucumberTest.java
@@ -0,0 +1,17 @@
+package nz.fox.craig.e2e;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/cucumber-setup.feature")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "nz.fox.craig.e2e.steps")
+public class RunCucumberTest {
+}
---
+++ src/test/resources/features/cucumber-setup.feature
@@ -0,0 +1,5 @@
+Feature: Cucumber setup
+
+  Scenario: Cucumber is working
+    Given the E2E test application is configured
+    Then Cucumber should execute successfully
